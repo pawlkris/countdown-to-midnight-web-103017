ac6a37153c60b3935897531682b123781bce6b43
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-18 21:51:38 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(number)
   end
   puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number >= 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end
+  puts "HAPPY NEW YEAR!"
+end
